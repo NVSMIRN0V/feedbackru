d32934232724fc64200b1406bff117599c76ddce
Nikita Smirnov <nvsmirnov00@yandex.ru>
2022-09-07 22:02:45 +0300

    Add static_root

--- feedbacksdotru/settings.py
+++ feedbacksdotru/settings.py
@@ -90,11 +90,16 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,  'static', 'media')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGOUT_REDIRECT_URL = 'login'
 LOGIN_REDIRECT_URL = 'fb-home'
+
 
